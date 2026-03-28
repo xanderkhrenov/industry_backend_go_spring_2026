59b253bea67dc35edea00f64e40950eba3adfdc1
Khrenov Alexander <AlexAndrKh.0305@gmail.com>
2026-03-28 11:14:04 +0300

    task_02 is done

--- tasks/task_02/solution.go
+++ tasks/task_02/solution.go
@@ -1,5 +1,26 @@
 package main
 
 func reverseRunes(s string) string {
-	// TODO
+	runes := []rune(s)
+	n := len(runes)
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+	return string(runes)
 }
+
+//// С использованием strings.Builder и defer
+//func reverseRunes(s string) string {
+//	var sb strings.Builder
+//	writeToBuilder(s, &sb)
+//	return sb.String()
+//}
+//
+//func writeToBuilder(s string, sb *strings.Builder) {
+//	var cnt int
+//	for _, r := range s {
+//		cnt++
+//		defer sb.WriteRune(r)
+//	}
+//	sb.Grow(cnt)
+//}
